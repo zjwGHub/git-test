422178b92978a062914fd522878162cc5e87352b
zjw <31456@qq.com>
2023-03-10 21:48:59 +0800

    push commit3

--- src/main/java/com/atguigu/gitTest/GitTest.java
+++ src/main/java/com/atguigu/gitTest/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hello test!");
         System.out.println("push test!2");
+        System.out.println("push test!1");
     }
 }
